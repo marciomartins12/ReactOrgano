04ae97dfcb43c88582bb34b580170fb22a792872
Marcio Jose Martins Camara <118389170+marciomartins12@users.noreply.github.com>
2023-08-09 11:07:28 -0300

    Delete src/componentes directory

--- src/componentes/Banner/Banner.js
+++
@@ -1,9 +0,0 @@
-import "./Banner.css"
-function Banner(){
-    return(
-        <div className="banner">
-            <img src="./imagens/banner.png" alt="imagem do banner"/>
-        </div>
-    )
-}
-export default Banner
