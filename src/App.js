6623f8cf4b4f49048db63eed762987d7ee91c087
Marcio Martins <mmarciom1martins635@gmail.com>
2023-08-14 15:30:11 -0300

    antes do favoritar

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ import Times from './components/times';
 function App() {
   const [player, setPlayer] = useState([])
 
-  const [times, setTimes ]= useState( [
+  const [times, setTimes] = useState([
     {
       funcao: "Iniciador",
       cor: "#c18ad1",
-      
+
     },
     {
       funcao: "Duelista",
       cor: "#0061ad",
-      
+
     },
     {
       funcao: "Controlador",
       cor: "#9b2d65",
-      
+
     },
     {
       funcao: "Sentinela",
       cor: "#09f6b6",
-      
-     
+
+
     },
   ])
   const [funcoes, setFuncoes] = useState([
@@ -39,23 +39,21 @@ function App() {
     setPlayer([...player, elemento])
   }
 
-function mudarCor(cor, nome){
-  setTimes(times.map((time)=>{
-    if(time.funcao === nome){
-      time.cor = cor
-    }
-  return time}) )
-}
-function aoDeletar(id){
-  setPlayer(player.filter((time)=> time.id !== id))
-}
-function atualizarListadeGrupos(nome, cor){
-  setTimes([...times, {funcao: nome, cor: cor}])
-  setFuncoes([...funcoes, nome]);
-  console.log(times)
-}
-
-
+  function mudarCor(cor, nome) {
+    setTimes(times.map((time) => {
+      if (time.funcao === nome) {
+        time.cor = cor
+      }
+      return time
+    }))
+  }
+  function aoDeletar(id) {
+    setPlayer(player.filter((time) => time.id !== id))
+  }
+  function atualizarListadeGrupos(nome, cor) {
+    setTimes([...times, { funcao: nome, cor: cor }])
+    setFuncoes([...funcoes, nome]);
+  }
   return (
     <section>
       <ImgHeader />
@@ -65,15 +63,15 @@ function atualizarListadeGrupos(nome, cor){
         receberPlayer={(elemento) => receberPlayer(elemento)}
       />
       {times.map((time, index) => {
-  return <Times
-    key={index}
-    timeCorP={time.cor}
-    timeNome={time.funcao}
-    mudarCor={mudarCor}
-    time={player.filter((p) => p.funcaoPlayer === time.funcao)}
-    aoDeletar={aoDeletar}
-  />
-})}
+        return <Times
+          key={index}
+          timeCorP={time.cor}
+          timeNome={time.funcao}
+          mudarCor={mudarCor}
+          time={player.filter((p) => p.funcaoPlayer === time.funcao)}
+          aoDeletar={aoDeletar}
+        />
+      })}
     </section>
   );
 }
