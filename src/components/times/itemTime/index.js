7d2a926400fd1b23bd382c51446deab9fba7fd0b
Marcio Martins <mmarciom1martins635@gmail.com>
2023-08-14 16:03:52 -0300

    .

--- src/components/times/itemTime/index.js
+++ src/components/times/itemTime/index.js
@@ -4,6 +4,9 @@ import {AiFillHeart} from "react-icons/ai"
 import "./Item.css"
 const Item = (props) => {
     console.log(props.favorito)
+    function tamanhoefuncao(){
+        props.favoritar(props.id)
+    }
     return (
         
             <div className="container-item">
@@ -20,7 +23,7 @@ const Item = (props) => {
                     <div className="div-nome-paragrafo">
                         <h3 style={{color:props.corP}}>{props.nome}</h3>
                         <h4 style={{color:props.corP}}>{props.funcao}</h4>
-                        {props.favorito? <AiFillHeart size={25} color="red" onClick={()=>props.favoritar(props.id)}/>: <AiOutlineHeart size={25} onClick={()=>props.favoritar(props.id)}/>}
+                        {props.favorito? <AiFillHeart size={25} color="red" onClick={()=>tamanhoefuncao()}/>: <AiOutlineHeart size={25} onClick={()=>tamanhoefuncao()}/>}
                         
                        
                     </div>
